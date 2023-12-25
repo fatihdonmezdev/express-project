7dfe0fdf35954397fa300a0e6ddbde536cf0a602
Fatih <fdonmez_1512@hotmail.com>
2023-12-25 16:11:31 +0300

    Added responses to post and get requests

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'This is GET request.', app: 'Fatih`s API' });
+});
+app.post('/', (req, res) => {
+  res.status(200).send('You can send POST req to this url.');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}....`);
+});
