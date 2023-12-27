ed554c0d4d589c4e5c29e1ef816371968b86656c
Fatih <fdonmez_1512@hotmail.com>
2023-12-27 17:07:26 +0300

    Added dynamic routes and dynamic queries for tours.

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+app.get('/api/v1/tours/:id', (req, res) => {
+  const paramId = req.params.id;
+  const tour = tours.find((tour) => tour.id === Number(paramId));
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -15,8 +23,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.status(200).send('Done');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  console.log(newTour);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 const port = 3000;
 app.listen(port, () => {
