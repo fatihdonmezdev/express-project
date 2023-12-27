5e2aec985b45d34106f79831ecef43f453e05d18
Fatih <fdonmez_1512@hotmail.com>
2023-12-27 13:08:36 +0300

    Added API for the get reqs

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'This is GET request.', app: 'Fatih`s API' });
-});
-app.post('/', (req, res) => {
-  res.status(200).send('You can send POST req to this url.');
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.status(200).send('Done');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on ${port}....`);
