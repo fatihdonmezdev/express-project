37c348e0692c8c821e1f315cea749dab07e73dd4
Fatih <fdonmez_1512@hotmail.com>
2023-12-31 21:41:48 +0300

    Added env variables to the server configuration

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const userRouter = require('./routes/userRoutes');
 
 // 1) MIDDLEWARES
 app.use(express.json());
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // 3) ROUTES
 
