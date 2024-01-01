2fe147ede945139451cd5816a5fa0031d809742b
Fatih <fdonmez_1512@hotmail.com>
2024-01-01 13:37:24 +0300

    Added DB connection to the server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
+const DB = process.env.DATABASE.replace(
+  '<password',
+  process.env.DATABASE_PASSWORD,
+);
+console.log(DB);
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
