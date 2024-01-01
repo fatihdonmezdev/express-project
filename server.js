6ab337760730b5b71170ca22ec31f6a3acd62698
Fatih <fdonmez_1512@hotmail.com>
2024-01-01 13:37:40 +0300

    Removed console.logs.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const DB = process.env.DATABASE.replace(
   '<password',
   process.env.DATABASE_PASSWORD,
 );
-console.log(DB);
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
