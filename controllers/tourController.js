2ae8529ececf70c31f8a0533a5def9d1eb25dab8
Fatih <fdonmez_1512@hotmail.com>
2024-01-01 15:46:57 +0300

    implemented MVC Architecture to the project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,68 +1,36 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (val * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+const Tour = require('./../models/tourModel')
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    data: { tours },
+    // results: tours.length,
+    // data: { tours },
   });
 };
 exports.getTourDetail = (req, res) => {
   const paramId = req.params.id * 1;
-  const tour = tours.find((tourname) => tourname.id === Number(paramId));
+  // const tour = tours.find((tourname) => tourname.id === Number(paramId));
 
   res.status(200).json({
     status: 'success',
-    data: { tour },
+    // data: { tour },
   });
 };
 
-exports.addNewTourMiddleware = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    res.status(400).json({ message: 'Name or price cant be empty!' });
-  } else {
-    next();
-  }
-};
+
 exports.addNewTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { ...req.body, id: newId };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
+  Tour.create({
+    name: 'Test'
+  })
+
       res.status(201).send({
         status: 'success',
         data: {
-          tour: newTour,
+          tour: '<AddedTour>',
         },
       });
-      if (err) {
-        res.status(400).json({
-          status: 'error',
-          data: {
-            err,
-          },
-        });
-      }
-    },
-  );
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -13,36 +13,6 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then((con) => {
   console.log(`DB connected succesfully to:  ${con.connection.name}`);
 });
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name.'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price.'],
-  },
-});
- 
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  rating: 4.2,
-  price: 335,
-});
-
-testTour
-  .save()
-  .then((doc) => console.log(doc))
-  .catch((err) => console.log(`Error occured: ${err}`));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on ${port}....`);
