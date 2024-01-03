9302fce859e15d1ce0fa8ea8b132eae7d99f00c0
Fatih <fdonmez_1512@hotmail.com>
2024-01-03 23:18:42 +0300

    Implemented new ways of queryies in the tours. such as: pagination,filtering.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,13 +5,47 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //BUILDING THE QUERY FIRST
+    //// 1A) FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = await Tour.find(req.query);
+    //// 1B)ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
 
+    /// 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3)Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    /// 4) Page and limiting amount.
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page doesnt exist.');
+      }
+    }
     // EXECUTE THE QUERY
+
     const tours = await query;
 
     //SEND RESPONSE
